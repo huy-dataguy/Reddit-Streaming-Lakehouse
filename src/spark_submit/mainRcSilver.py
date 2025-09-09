705a3d3bda27e31fb23181b360603250a4d6f381
huy-dataguy <quochuy.working@gmail.com>
2025-09-09 16:23:59 +0700

    fix bug silver read transform and write data with forechbatch

--- src/spark_submit/mainRcSilver.py
+++ src/spark_submit/mainRcSilver.py
@@ -6,22 +6,28 @@ from pyspark.sql.functions import *
 from transformer.silverSubTransfomer import SubmissionTransformer
 from transformer.silverCmtTransformer import CommentTransformer
 
-spark = (SparkSession.builder
-        .appName("SilverTransformerComment")
-        .enableHiveSupport()
-        .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-        .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
-        .config("spark.sql.catalog.spark_catalog.type", "hive")
-        .config("spark.sql.catalog.spark_catalog.uri", "thrift://hive-metastore:9083")
-        .config("spark.sql.catalog.spark_catalog.warehouse", "s3a://checkpoint/")
-        .config("spark.hadoop.fs.s3a.endpoint", "http://minio1:9000")
-        .config("spark.hadoop.fs.s3a.access.key", "minio")
-        .config("spark.hadoop.fs.s3a.secret.key", "mypassword")
-        .config("spark.hadoop.fs.s3a.path.style.access", "true")
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
-        .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
-        .getOrCreate())
+from utils.getSparkConfig import getSparkConfig
+
+
+spark = getSparkConfig("SilverTransformerComment")
 
 commentTransformer = CommentTransformer(spark)
-commentTransformer.transform("spark_catalog.bronze.reddit_comment", "spark_catalog.silver.reddit_comment", checkpointPath="s3a://checkpoint/lakehouse/silver/reddit_comment/", streaming=True)
+
+pathOut="spark_catalog.silver.reddit_comment"
+
+dfBatch = commentTransformer.readData("spark_catalog.bronze.reddit_comment", streaming=True)
+
+def processRC(batch_df, batch_id):
+        df = commentTransformer.convertTimestamp(batch_df)
+        df = commentTransformer.markAuthorDeleted(df) 
+        df = commentTransformer.markBodyRemoved(df, body="body", newCol="cmtDeleted")
+        df = commentTransformer.normalizeParentId(df)
+        df = commentTransformer.normalizeLinkId(df)
+        df = commentTransformer.markModComments(df)
+        commentTransformer.writeData(df, pathOut)
+
+query = commentTransformer.writeData(df=dfBatch, pathOut=pathOut, checkpointPath="s3a://checkpoint/lakehouse/silver/reddit_comment/", streaming=True, batchFunc=processRC)
+
+query.awaitTermination()
+
+
--- src/spark_submit/mainRsSilver.py
+++ src/spark_submit/mainRsSilver.py
@@ -5,25 +5,25 @@ from pyspark.sql.functions import *
 
 from transformer.silverSubTransfomer import SubmissionTransformer
 from transformer.silverCmtTransformer import CommentTransformer
+from utils.getSparkConfig import getSparkConfig
 
-spark = (SparkSession.builder
-        .appName("SilverTransformerSubmission")
-        .enableHiveSupport()
-        .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-        .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
-        .config("spark.sql.catalog.spark_catalog.type", "hive")
-        .config("spark.sql.catalog.spark_catalog.uri", "thrift://hive-metastore:9083")
-        .config("spark.sql.catalog.spark_catalog.warehouse", "s3a://checkpoint/")
-        .config("spark.hadoop.fs.s3a.endpoint", "http://minio1:9000")
-        .config("spark.hadoop.fs.s3a.access.key", "minio")
-        .config("spark.hadoop.fs.s3a.secret.key", "mypassword")
-        .config("spark.hadoop.fs.s3a.path.style.access", "true")
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
-        .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
-        .getOrCreate())
+
+spark = getSparkConfig("SilverTransformerSubmission")
 
 
 submissionTransformer = SubmissionTransformer(spark)
-submissionTransformer.transform("spark_catalog.bronze.reddit_submission", "spark_catalog.silver.reddit_submission", checkpointPath="s3a://checkpoint/lakehouse/silver/reddit_submission/", streaming=True)
+pathOut = "spark_catalog.silver.reddit_submission"
 
+dfBatch = submissionTransformer.transform("spark_catalog.bronze.reddit_submission", streaming=True)
+
+
+def processRS(batch_df, batch_id):
+        df = submissionTransformer.convertTimestamp(batch_df)
+        df = submissionTransformer.markAuthorDeleted(df) 
+        df = submissionTransformer.markBodyRemoved(df)
+        df = submissionTransformer.markSpamPost(df)
+        submissionTransformer.writeData(df, pathOut)
+    
+query = submissionTransformer.writeData(df=dfBatch, pathOut=pathOut, checkpointPath="s3a://checkpoint/lakehouse/silver/reddit_submission/", streaming=True, batchFunc=processRS)
+
+query.awaitTermination()
