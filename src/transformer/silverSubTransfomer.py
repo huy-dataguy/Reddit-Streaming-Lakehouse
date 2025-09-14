1f31885a7f2433c9a4229d5f15c8637ec59a3c94
huy-dataguy <quochuy.working@gmail.com>
2025-09-14 21:14:17 +0700

    check is post spam update

--- src/transformer/silverSubTransfomer.py
+++ src/transformer/silverSubTransfomer.py
@@ -7,18 +7,22 @@ from transformer.silverBaseTransformer import BaseTransformer
 class SubmissionTransformer(BaseTransformer):
     def __init__(self, sparkSession):
         super().__init__(sparkSession)
-
+ 
+    
     def markSpamPost(self, df, titleCol="title", urlCol="url", domainCol="domain", subredditCol="subreddit", newCol="isPostSpam"):
         words = F.split(F.lower(F.col(titleCol)), " ")
         numMatched = F.size(
-        F.expr(f"filter(split(lower({titleCol}), ' '), word -> instr(lower({urlCol}), word) > 0)")
-        )        
-        totalWord = F.size(words)
-        matchPercent = (numMatched * 100) / totalWord
-        isPostSpam = (matchPercent >= 0.6) & (
-        F.col(domainCol) != F.concat(F.lit("self."), F.col(subredditCol))
+            F.expr(f"filter(split(lower({titleCol}), ' '), word -> instr(lower({urlCol}), word) > 0)")
         )
-        return df.withColumn("matchPercent", matchPercent).withColumn(newCol, isPostSpam)   
+        totalWord = F.size(words)
+        matchPercent = F.when(totalWord == 0, F.lit(0)).otherwise((numMatched * 100.0) / totalWord)
+
+        isPostSpam = (matchPercent >= 60) & (
+            F.col(domainCol) != F.concat(F.lit("self."), F.col(subredditCol))
+        )
+
+        return df.withColumn("matchPercent", matchPercent).withColumn(newCol, isPostSpam)
+
          
     def transform(self, pathIn, format="iceberg", streaming=False):
         df = self.readData(pathIn, format, streaming=streaming)
