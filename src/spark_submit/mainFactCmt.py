4e287293706952ad630fad146335520c157f5adf
huy-dataguy <quochuy.working@gmail.com>
2025-09-16 13:04:12 +0700

    seprate factCmt to another file

--- src/spark_submit/mainFactCmt.py
+++ src/spark_submit/mainFactCmt.py
@@ -26,42 +26,16 @@ gold = GoldTransformer(spark)
 dfSubNew=gold.readData(pathIn=pathRS, streaming=True)
 dfCmtNew=gold.readData(pathIn=pathRC, streaming=True)
 
-
-# def processFactPostActivity(batch_df, batch_id):
-#         dTime = gold.readData(pathDTime)
-#         dAuthor = gold.readData(pathDAuthor)
-#         dSubreddit = gold.readData(pathDSubreddit)
-#         dPostType = gold.readData(pathDPostType)
-#         dSentiment = gold.readData(pathDSentiment)
-#         dCmt = gold.readData(pathDComment)
-
-
-#         factPost = gold.createFactPostActivity(dTime, dAuthor, dSubreddit, dPostType, dSentiment, dCmt, batch_df)
-#         gold.writeData(factPost,pathOut= pathFPost)
-        
-
-# qFPost = (dfSubNew.writeStream
-#     .option("checkpointLocation", "s3a://checkpoint/lakehouse/gold/factPostActivity/")
-#     .foreachBatch(processFactPostActivity)
-#     .start()
-# )
-
-
 def processFactCommentActivity(batch_df, batch_id):
-    try:
-        dTime = gold.readData(pathDTime)
-        dAuthor = gold.readData(pathDAuthor)
-        dSubreddit = gold.readData(pathDSubreddit)
-        dPost= gold.readData(pathDPost)
-        dSentiment = gold.readData(pathDSentiment)
+    dTime = gold.readData(pathDTime)
+    dAuthor = gold.readData(pathDAuthor)
+    dSubreddit = gold.readData(pathDSubreddit)
+    dPost= gold.readData(pathDPost)
+    dSentiment = gold.readData(pathDSentiment)
 
-        factComment = gold.createFactCommentActivity(dTime, dAuthor, dSubreddit, dPost, dSentiment, batch_df)
-        gold.writeData(factComment, pathOut=pathFCmt)
+    factComment = gold.createFactCommentActivity(dTime, dAuthor, dSubreddit, dPost, dSentiment, batch_df)
+    gold.writeData(factComment, pathOut=pathFCmt)
 
-        print("heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy halo")
-
-    except Exception as e:
-        print(f"Error batch {batch_id}: {e}")
 
 
 qFCmt = (dfCmtNew.writeStream
