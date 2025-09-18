7cdcc64f042502d2c5cf62e3081d123edaa85685
huy-dataguy <quochuy.working@gmail.com>
2025-09-18 15:37:26 +0700

    script to build image, start container

--- scripts/startServices.sh
+++ scripts/startServices.sh
@@ -1,69 +1,55 @@
 #!/bin/bash
 
-# --- 1. Build sparkbase image ---
-echo "--- Building sparkbase image... ---"
+# 1. build sparkbase image for spark on yarn
+echo "Building sparkbase image"
 docker build -t sparkbase -f docker/spark_on_YARN/base.dockerfile .
+echo "sparkbase image built successfully"
 
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to build sparkbase image. Exiting."
-  exit 1
-fi
-echo "sparkbase image built successfully."
+cd docker
 
-# --- 2. Build and run Spark on YARN cluster ---
-echo "--- Building and running Spark on YARN cluster... ---"
-cd docker/
+# 2. create network
+echo "Creating Docker network 'reddit_network'"
+docker network create reddit_network
 
-# Kiểm tra và tạo network nếu chưa tồn tại
-if ! docker network ls | grep -q reddit_network; then
-  echo "Creating Docker network 'reddit_network'..."
-  docker network create reddit_network
-fi
+# 3. build image for all service
+docker compose -f spark.compose.yml build &
+docker compose -f kafka.compose.yaml build &
+docker compose -f trino.compose.yaml build &
+docker compose -f superset.compose.yml build &
+curl -Lf 'https://airflow.apache.org/docs/apache-airflow/3.0.6/docker-compose.yaml' -o airflow.compose.yaml
 
-docker compose -f spark.compose.yml build
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to build Spark images. Exiting."
-  exit 1
-fi
 
-docker compose -f spark.compose.yml up -d
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to start Spark containers. Exiting."
-  exit 1
-fi
-echo "Spark on YARN cluster is running."
+wait
 
-# --- 3. Build and run MinIO ---
-echo "--- Building and running MinIO... ---"
-docker compose -f minio.compose.yaml up -d
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to start MinIO container. Exiting."
-  exit 1
-fi
-echo "MinIO is running."
+mkdir -p ../config/airflow/dags ../config/airflow/logs ../config/airflow/plugins ../config/airflow/config
+export AIRFLOW_PROJ_DIR="../config/airflow"
+AIRFLOW_BASE="../config/airflow"
+echo -e "AIRFLOW_UID=$(id -u)" > "$AIRFLOW_BASE/.env"
 
-# --- 4. Build and run Hive Metastore ---
-echo "--- Building and running Hive Metastore... ---"
-docker compose -f hive.compose.yaml up -d
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to start Hive Metastore container. Exiting."
-  exit 1
-fi
-echo "Hive Metastore is running."
 
-# --- 5. Build and run Kafka cluster ---
-echo "--- Building and running Kafka cluster... ---"
-docker compose -f kafka.compose.yaml build
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to build Kafka images. Exiting."
-  exit 1
-fi
 
-docker compose -f kafka.compose.yaml up -d
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to start Kafka containers. Exiting."
-  exit 1
-fi
-echo "Kafka cluster is running."
+# 4. start container
+echo "starting all services..."
+docker compose -f spark.compose.yml up -d &
+docker compose -f minio.compose.yaml up -d &    
+docker compose -f hive.compose.yaml up -d &
+docker compose -f kafka.compose.yaml up -d &
+docker compose -f superset.compose.yml up -d &
+docker compose -f trino.compose.yaml up -d &
+docker compose -f airflow.compose.yaml up -d
 
-echo "--- All services have been started successfully! ---"
+wait
+
+echo "All services have been started successfully"
+
+# 5. create topics
+echo "Create topics kafka"
+echo "Waiting Kafka broker to be ready..."
+sleep 20
+docker exec -it kafka1 bash -c "su kafka_user -c '~/kafka/bin/kafka-topics.sh --create --topic redditSubmission --bootstrap-server kafka1:9092 --replication-factor 2 --partitions 6'"
+docker exec -it kafka1 bash -c "su kafka_user -c '~/kafka/bin/kafka-topics.sh --create --topic redditComment --bootstrap-server kafka1:9092 --replication-factor 2 --partitions 6'"
+# 6
+cd ..
+
+cd config/superset
+source ./setup_superset.sh
